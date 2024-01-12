3cfd15f4fa0ff950b05b931d5ed97985fa1260ed
Mener7584 <138903594+Mener7584@users.noreply.github.com>
2024-01-12 14:50:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 "use strict";
 
 (() => {
-	const burger = document.querySelector(".header__burger");
-	const menu = document.querySelector(".menu-header");
+    const burger = document.querySelector(".menu-header__burger");
+    const menu = document.querySelector(".menu-header");
 
-	if (!(menu instanceof HTMLElement) || !(burger instanceof HTMLButtonElement)) {
-		return;
-	}
+    if (!(menu instanceof HTMLElement) || !(burger instanceof HTMLButtonElement)) {
+        return;
+    }
 
-	burger.addEventListener("click", () => {
-		burger.classList.toggle("active");
-		menu.classList.toggle("active");
-		document.body.classList.toggle("lock");
-	});
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        menu.classList.toggle("active");
+        document.body.classList.toggle("lock");
+    });
 })();
